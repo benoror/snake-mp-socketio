0f46caefbae3a710da65a2d3636edce9aea08f7f
Ben Orozco <benoror@gmail.com>
2016-10-27 11:37:19 -0500

    feat: setup basic socket.io func

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-    res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('key', (key) => {
+    console.log(key);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
 });
 
