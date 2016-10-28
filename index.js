b0c2ddc754d274266c08eafe20efb5f98808e448
Ben Orozco <benoror@gmail.com>
2016-10-27 23:09:06 -0500

    feat: draw apples

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+// Use ES6
+"use strict";
 
+// Express & Socket.io deps
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+// Key maps
 const KEYS = {
   up: 38,
   right: 39,
@@ -9,50 +14,42 @@ const KEYS = {
   left: 37
 };
 
+// Handy remove method for arrays
 Array.prototype.remove = function(e) {
-  var t, _ref;
-  if ((t = this.indexOf(e)) > -1) {
-    return ([].splice.apply(this, [t, t - t + 1].concat(_ref = [])), _ref);
+  var p, _;
+  if ((p = this.indexOf(e)) > -1) {
+    return ([].splice.apply(this, [p, p - p + 1].concat(_ = [])), _);
   }
 };
 
-var autoId = 0;
+// Remote players
 var players = [];
 
-Snake = (function() {
-  function Snake(id, dir, x, y) {
-    this.id = id;
-    this.dir = dir;
+class Snake {
+  constructor(dir, x, y) {
+    this.dir = dir; //direction
     this.x = x;
     this.y = y;
   }
 
-  Snake.prototype.changeDirection = function(key) {
+  changeDirection(key) {
     switch (key) {
       case KEYS.up:
-        if (this.dir !== 'down') {
-          this.dir = 'up';
-        }
-        break;
+        if (this.dir !== 'down')
+          this.dir = 'up'; break;
       case KEYS.right:
-        if (this.dir !== 'left') {
-          this.dir = 'right';
-        }
-        break;
+        if (this.dir !== 'left')
+          this.dir = 'right'; break;
       case KEYS.down:
-        if (this.dir !== 'up') {
-          this.dir = 'down';
-        }
-        break;
+        if (this.dir !== 'up')
+          this.dir = 'down'; break;
       case KEYS.left:
-        if (this.dir !== 'right') {
-          this.dir = 'left';
-        }
-        break;
+        if (this.dir !== 'right')
+          this.dir = 'left'; break;
     }
   }
 
-  Snake.prototype.move = function() {
+  move() {
     switch(this.dir) {
       case 'right':
         this.x++; break;
@@ -64,52 +61,51 @@ Snake = (function() {
         this.y++; break;
     }
 
+    // Boundaries
     if(this.x > 30-1) this.x = 0;
     if(this.x < 0) this.x = 30-1;
     if(this.y > 30-1) this.y = 0;
     if(this.y < 0) this.y = 30-1;
   }
+}
 
-  return Snake;
-})();
+/*
+ * Serve client
+ */
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (client) => {
-  const id = autoId++;
-  var player = new Snake(id, 'right', 0, 1);
-  players.push(player);
-
-  client.on('auth', (cb) => {
-    console.log('>>> player ' + player.id + ' authenticated');
-    cb({ id });
-  });
-
-  client.on('key', (key) => {
-    player.changeDirection(key);
-  });
-
-  client.on('disconnect', () => {
-    players.remove(player);
-    console.log('<<< player ' + player.id + ' disconnected');
-  });
-});
-
-var tick = function() {
-  players.forEach((p) => { p.move(); });
-  io.emit('state', players.map((p) => {
-    return {
-      x: p.x,
-      y: p.y
-    }
-  }));
-};
-
-setInterval(tick, 100);
-
 http.listen(3000, () => {
   console.log('listening on *:3000');
 });
+
+/*
+ * Listen for incoming clients
+ */
+io.on('connection', (client) => {
+  var player = new Snake('right', 0, 1);
+  players.push(player);
+
+  // Receive keystrokes
+  client.on('key', (key) => {
+    // and change direction accordingly
+    player.changeDirection(key);
+  });
+
+  // Remove player on disconnect
+  client.on('disconnect', () => {
+    players.remove(player);
+  });
+});
+
+// Main loop
+setInterval(() => {
+  players.forEach((p) => { p.move(); });
+  io.emit('state', {
+    players: players.map((p) => ({ x: p.x, y: p.y })),
+    apples: [{x: 10, y: 10}]
+  });
+}, 100);
 
